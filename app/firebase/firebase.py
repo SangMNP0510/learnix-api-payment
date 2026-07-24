371a1d94196fe650a21f198d0c37c8b229026db4
Nailengg <sang.pham@vgl.io.vn>
2026-07-24 17:49:14 +0700

    Always return 200 for PayOS webhook

--- app/firebase/firebase.py
+++ app/firebase/firebase.py
@@ -1,28 +1,50 @@
+import json
+import os
 from pathlib import Path
 
 import firebase_admin
 from firebase_admin import credentials, firestore
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
 
-credential_path = os.getenv("FIREBASE_CREDENTIALS")
+if firebase_credentials is None:
+    raise RuntimeError(
+        "FIREBASE_CREDENTIALS is not configured."
+    )
 
-if credential_path is None:
-    raise RuntimeError("FIREBASE_CREDENTIALS is not configured.")
+# ===== Render =====
+# FIREBASE_CREDENTIALS chứa nguyên JSON
 
-SERVICE_ACCOUNT_PATH = BASE_DIR / credential_path
+if firebase_credentials.strip().startswith("{"):
 
-if not SERVICE_ACCOUNT_PATH.exists():
-    raise FileNotFoundError(
-        f"Firebase credential not found: {SERVICE_ACCOUNT_PATH}"
+    cred = credentials.Certificate(
+        json.loads(firebase_credentials)
+    )
+
+# ===== Local =====
+# FIREBASE_CREDENTIALS là đường dẫn file
+
+else:
+
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+    service_account_path = (
+        BASE_DIR / firebase_credentials
+    )
+
+    if not service_account_path.exists():
+        raise FileNotFoundError(
+            f"Firebase credential not found: {service_account_path}"
+        )
+
+    cred = credentials.Certificate(
+        str(service_account_path)
     )
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(str(SERVICE_ACCOUNT_PATH))
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
