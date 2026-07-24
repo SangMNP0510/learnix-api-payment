34292519e8c43e423e546fbae6ebf3c3fb979bae
Nailengg <sang.pham@vgl.io.vn>
2026-07-24 15:46:36 +0700

    Use Firebase credentials from environment

--- app/config/settings.py
+++ app/config/settings.py
@@ -7,6 +7,10 @@ PAYOS_CLIENT_ID = os.getenv("PAYOS_CLIENT_ID")
 PAYOS_API_KEY = os.getenv("PAYOS_API_KEY")
 PAYOS_CHECKSUM_KEY = os.getenv("PAYOS_CHECKSUM_KEY")
 
-RETURN_URL = "http://localhost:3000/payment-success"
+RETURN_URL = os.getenv(
+    "RETURN_URL"
+)
 
-CANCEL_URL = "http://localhost:3000/payment-cancel"
+CANCEL_URL = os.getenv(
+    "CANCEL_URL"
+)
