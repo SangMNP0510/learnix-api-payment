e07b553aecf6cf1177b5f9acb0eb2e2bf580ff6d
Nailengg <sang.pham@vgl.io.vn>
2026-07-24 15:33:05 +0700

    Use Firebase credentials from environment

--- app/firebase/firebase.py
+++ app/firebase/firebase.py
@@ -1,21 +1,23 @@
+import json
+import os
+
 import firebase_admin
 
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-from pathlib import Path
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-SERVICE_ACCOUNT_PATH = (
-    BASE_DIR
-    / "credentials"
-    / "firebase-service-account.json"
+from firebase_admin import (
+    credentials,
+    firestore,
 )
 
-cred = credentials.Certificate(str(SERVICE_ACCOUNT_PATH))
+cred = credentials.Certificate(
+    json.loads(
+        os.environ[
+            "FIREBASE_CREDENTIALS"
+        ]
+    )
+)
 
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(
+    cred
+)
 
 db = firestore.client()
